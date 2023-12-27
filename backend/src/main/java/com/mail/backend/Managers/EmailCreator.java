4461f87360b2a5849518a8053da27fa605e7493f
Ahmad Hassan <43483116+AhmadHassan1001@users.noreply.github.com>
2023-12-28 01:09:06 +0200

    Queues (#12)
    * upload attachment v1
    
    * Move file to Attachs folder after creation
    
    * Showing files name as a response
    
    * Attach files to email by id
    
    * Some changes
    
    * merge with main
    
    * Correcting an end-point
    
    * Downloading from server v1
    
    * Solving some Errors
    
    * Addaning Attachment to Manager
    
    * Solving some issues
    
    * add controller to attachements and finalze send
    
    * finalize send email
    
    * sort using  pq
    
    ---------
    
    Co-authored-by: Ab00dMD <abdalrhmanmohamed94@gmail.com>
    Co-authored-by: AbdElRahman Osama <52374998+AbdElRahmanOsama182@users.noreply.github.com>
    Co-authored-by: A_DEIF <63081270+Ab00dMD@users.noreply.github.com>

--- backend/src/main/java/com/mail/backend/Managers/EmailCreator.java
+++ backend/src/main/java/com/mail/backend/Managers/EmailCreator.java
@@ -2,10 +2,14 @@ package com.mail.backend.Managers;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
 
 import com.mail.backend.Managers.*;
+import com.mail.backend.Models.Afile.Afile;
 import com.mail.backend.Models.Contact.Contact;
 import com.mail.backend.Models.Email.Email;
 import com.mail.backend.Models.Folder.Folder;
@@ -18,11 +22,21 @@ public class EmailCreator {
     public static Email createAndSend(Email email,User user){
         FolderManager folderManager = (FolderManager) ManagerFactory.getManager("FolderManager");
         EmailManager emailManager = (EmailManager) ManagerFactory.getManager("EmailManager");
+        AttachmentManager attachmentManager =(AttachmentManager) ManagerFactory.getManager("AttachmentManager");
 
     
         // add fields
         email.setFromUserId(user.getUsername());
         email.setSendDate(new Date());
+        
+        // attachements
+        for(int i=0;i<email.getAttachments().size();i++){
+            Afile attachment=email.getAttachments().get(i);
+            attachment = attachmentManager.get(attachment.getId());
+            email.getAttachments().set(i, attachment);
+            System.out.println("attachment.getPaths()");
+            System.out.println(attachment.getPaths());
+        }
         
         // add to outbox folder
         Folder outboxFolder = folderManager.getUserFolderByName(user.getUsername(), "Sent");
@@ -38,24 +52,37 @@ public class EmailCreator {
         System.out.println("outboxFolder.getEmails()");
         System.out.println(outboxFolder.getEmails());
 
-        // attachements
 
+        // send
+        Queue<Contact> contacts = new LinkedList<Contact>();
 
-        // clone and send
+        for(Contact contact:email.getTo()){
+            contacts.add(contact);
+        }
+        final Email finalEmail = email;
+        CompletableFuture.runAsync(() -> {
+            sendEmails(finalEmail,contacts);
+        });
+        
+        return email;
+    }
 
-        for(Contact contact : email.getTo()){
-            // clone
+    public static void sendEmails(Email email,Queue<Contact> contacts){
+        EmailManager emailManager = (EmailManager) ManagerFactory.getManager("EmailManager");
+        FolderManager folderManager = (FolderManager) ManagerFactory.getManager("FolderManager");
+        UserManager userManager = (UserManager) ManagerFactory.getManager("UserManager");
+        System.out.println("sendEmails to:"+contacts.size()+" contacts");
+        System.out.println(contacts);
+        while(!contacts.isEmpty()){
+            Contact contact = contacts.poll();
             Email sentEmail = emailManager.createEmail(email.readEmail());
 
-            UserManager userManager = (UserManager) ManagerFactory.getManager("UserManager");
             User toUser = userManager.getUserByEmail(contact.getEmails().get(0));
             // send
             Folder inboxFolder = folderManager.getUserFolderByName(toUser.getUsername(), "Inbox");
             sentEmail.setFolderId(inboxFolder.getId());
             folderManager.addEmail(inboxFolder.getId(),sentEmail.getId() );
-
         }
 
-        return email;
     }
 }
